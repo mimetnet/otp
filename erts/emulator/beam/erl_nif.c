893337005191a49c39eaeaf4c061bc012c08e46e
Matthew Metnetsky <mmetnetsky@kmbs.konicaminolta.us>
2023-11-15 10:16:59 -0500

    chore: replace fprintf w/ erts_fprintf

--- erts/emulator/beam/erl_nif.c
+++ erts/emulator/beam/erl_nif.c
@@ -3179,14 +3179,14 @@ void* enif_dlopen(const char* lib,
     void* handle;
     void* init_func;
 
-	fprintf(stderr, "enif_dlopen: %s\n", lib);
+    erts_fprintf(stderr, "enif_dlopen: %s\n", lib);
 
     if (erts_sys_ddll_open(lib, &handle, &errdesc) == ERL_DE_NO_ERROR) {
 	if (erts_sys_ddll_load_nif_init(handle, &init_func, &errdesc) == ERL_DE_NO_ERROR) {
-        fprintf(stderr, "enif_dlopen: %s call nif_init\n", lib);
+	    erts_fprintf(stderr, "enif_dlopen: %s call nif_init\n", lib);
 	    erts_sys_ddll_call_nif_init(init_func);
 	} else {
-        fprintf(stderr, "enif_dlopen: %s failed\n", lib);
+        erts_fprintf(stderr, "enif_dlopen: %s failed\n", lib);
     }
     }
     else {
